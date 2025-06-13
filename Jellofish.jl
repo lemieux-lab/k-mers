405bb994c6bad229f7d300a728e64b27cefc2393
Gortaf <nicolas.jacquin@gmail.com>
2025-06-13 11:10:11 -0400

    Lab Meeting Commit

--- Jellofish.jl
+++ Jellofish.jl
@@ -7,6 +7,7 @@ using BioSymbols
 using Kmers
 using ProgressMeter
 using Base.Threads
+using EzXML
 
 struct Node{Sb<:BioSymbol}
     value::Sb
@@ -63,6 +64,15 @@ function k_merize(sequence::LongSequence{Ab}; K::Int) where {Ab<:Alphabet}
     return to_return
 end
 
+function k_merize(sequence::LongAA; K::Int)
+    length(sequence) < K && error("Read of size $(length(sequence)) too small for $K-mers")
+    to_return = Kmer{JuBox.AAAlphabet, K}[]
+    for i in 1:(length(sequence)-K+1)
+        push!(to_return, Kmer{JuBox.AAAlphabet, K}(sequence[i:i+K-1]))
+    end
+    return to_return
+end
+
 function jello_hash(fastq::String; K::Int)
     hash = Dict{UInt64, UInt32}()
     wcl = countlines(fastq)
@@ -86,9 +96,82 @@ function jello_hash(fastq::String; K::Int)
     return hash
 end
 
+function JuBox.Kct(hash_table::Dict{UInt64, UInt32}, K::Int, A::Ab=DNAAlphabet{2}) where {Ab<:Type{<:Alphabet}}
+
+    count_type = UInt32
+    symbol_size = BioSequences.bits_per_symbol(A())
+    required_chunks = ceil(Int64, K/(64/symbol_size))  # On 64 bits
+
+    nb = Vector{count_type}()
+    kct = Kct(1, K)
+
+    @showprogress desc="parsing hash table into KCT..." for (k_bits, k_val) in hash_table
+        tmp_seq = Kmer{A, K, required_chunks}(Kmers.unsafe, (k_bits,))
+        push!(kct.table, JuBox.KRecord(tmp_seq, UInt32(0)))
+        push!(nb, k_val)
+    end
+    
+    JuBox._dedup(kct, nb)
+    sort!(kct)
+    return kct
+end
+
+# abstract type Extension{E} end
+
+struct Fastq
+    iterable::IOStream
+end
+
+struct Mzid
+    iterable::Vector{EzXML.Node}
+end
+
+function Base.iterate(seqfile::Fastq, state=nothing)
+    eof(seqfile.iterable) && return nothing
+    readline(seqfile.iterable)
+    seq = readline(seqfile.iterable)
+    readline(seqfile.iterable)
+    readline(seqfile.iterable)
+    return seq, state
+end
+
+function Base.iterate(seqfile::Mzid, state::Int=1)
+    return state < length(seqfile.iterable) ? (seqfile.iterable[state].content, state+1) : nothing
+end
+
+stream(::Type{Val{:fastq}}, path::String) = Fastq(open(path, "r"))
+stream(::Type{Val{:mzid}}, path::String) = Mzid(findall("//*[local-name()='Seq']", readxml(path)))
+stream(path::String) = stream(Val{Symbol(split(path, ".")[end])}, path)
+
+function chunk_stream(file::String, K::Int, merge_queue::Channel{Dict{UInt64, UInt32}}, chunking::Int=1_000_000)
+    counting_tasks = Vector{Task}()
+    current_chunk = String[]
+    i = 0
+    io = stream(file)
+
+    progress = ProgressUnknown(desc = "Processing $chunking k-mers chunks...")
+    for seq in io
+        push!(current_chunk, seq)
+        i += 1
+
+        if i % chunking == 0
+            to_process = deepcopy(current_chunk)
+            push!(counting_tasks, @spawn process_thread(to_process, K, merge_queue))
+            current_chunk = String[]
+            next!(progress; showvalues=[
+            ("active counting tasks", length(filter(x->x.state!=:done, counting_tasks))),
+            ("items in merge queue", merge_queue.n_avail_items)
+            ])
+        end
+    end
+    if !isempty(current_chunk)
+        push!(counting_tasks, @spawn process_thread(current_chunk, K, merge_queue))
+    end
+    wait.(counting_tasks)
+end
+
 function jello_threaded_hash(fastq::String, K::Int, chunking::Int=1_000_000, queue_size::Int=32)
-    @time lines = readlines(open(fastq, "r"))[2:4:end]
-    chunks = [lines[i:min(i+chunking-1, end)] for i in 1:chunking:length(lines)]
+
     merge_queue = Channel{Dict{UInt64, UInt32}}(queue_size)
     mother_hash = Dict{UInt64, UInt32}()
     merger = @async try
@@ -99,10 +182,16 @@ function jello_threaded_hash(fastq::String, K::Int, chunking::Int=1_000_000, que
         @error "Merger task crashed" exception=(e, catch_backtrace())
     end
 
-    progress = Progress(length(chunks), desc = "Processing $chunking k-mers chunks...")
-    # Worker threads, building child Kcts asynchronously
-    @threads for chunk in chunks
-        hash = Dict{UInt64, UInt32}()
+    chunk_stream(fastq, K, merge_queue, chunking)
+
+    close(merge_queue)
+    wait(merger)
+    return mother_hash
+end
+
+function process_thread(chunk::Vector{String}, K::Int, merge_queue::Channel{Dict{UInt64, UInt32}})
+    hash = Dict{UInt64, UInt32}()
+    try 
         for l in chunk
             seq = bioseq(l)
             kmers = k_merize(seq, K=K)
@@ -113,16 +202,14 @@ function jello_threaded_hash(fastq::String, K::Int, chunking::Int=1_000_000, que
                 hash[kmer.data[1]] += 1
             end
         end
-
-        put!(merge_queue, hash)
-        next!(progress; showvalues=[
-                ("items in merge queue", merge_queue.n_avail_items)
-                ])
+    catch e
+        @error "counting thread task crashed during hash building" exception=(e, catch_backtrace())
     end
-    
-    close(merge_queue)
-    wait(merger)
-    return mother_hash
+    try
+        put!(merge_queue, hash)
+    catch e
+        @error "counting thread task crashed during merge queue put" exception=(e, catch_backtrace())
+    end
 end
 
 function jello_trie(fastq::String, Sb::Type{<:BioSymbol}=DNA; K::Int)
@@ -150,7 +237,6 @@ end
 function collapse(kct::Kct)
 
 end
-
 
 function jello_kct_chunk(fastq::String; K::Int, chunking::Int=1_000_000, queue_size::Int=32)
     lines = readlines(open(fastq, "r"))[2:4:end]
